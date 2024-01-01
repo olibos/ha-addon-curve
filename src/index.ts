e133d04cd0f90ba5fdb518a1985a144d50c741f0
Olivier Bossaer <olivier.bossaer@gmail.com>
2024-01-01 14:16:14 +0100

    Fix: This framework implicitly discloses version information by default. Make sure it is safe here.

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,10 @@
-import {version} from "./package.json";
+import { version } from "./package.json";
 import express from "express";
 
 const app = express();
 const port = process.env.PORT ?? 3000;
 
+app.disable("x-powered-by");
 app.get("/", (_, res) => {
   res.send(`Hello ha-addon-curve ${version}!`);
 });
