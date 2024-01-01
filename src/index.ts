b0d05aaacf896ccfb80fb856d9a77b638afb7540
Olivier Bossaer <olivier.bossaer@gmail.com>
2024-01-01 14:12:13 +0100

    Setup express

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,13 @@
-import {version} from './package.json';
-console.log(`Hello ha-addon-curve ${version}!`);
+import {version} from "./package.json";
+import express from "express";
+
+const app = express();
+const port = process.env.PORT ?? 3000;
+
+app.get("/", (_, res) => {
+  res.send(`Hello ha-addon-curve ${version}!`);
+});
+
+app.listen(port, () => {
+  console.log(`Listening on http://localhost:${port} ...`);
+});
